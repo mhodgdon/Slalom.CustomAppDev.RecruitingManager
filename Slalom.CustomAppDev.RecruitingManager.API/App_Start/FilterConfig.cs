de537e0a0a67ea63d067bf01ff55c2409125bfbb
mhodgdon <mhodgdon@gmail.com>
2013-05-24 15:47:16 -0400

    Adding solution structure to Source.

---
+++ Slalom.CustomAppDev.RecruitingManager.API/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Slalom.CustomAppDev.RecruitingManager.API
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
