d9b1853343e0d1cb917f7b94b0d79414baae27d1
Brendan Schoch <brendans@slalom.com>
2013-05-31 09:35:57 -0400

    Added New Views, new JS, new CSS to .Web project
    Added: twitter bootstrap css and js files, added partial view for card
    view, adjusted index and layout.cshtml pages. adjusted site.css

--- Slalom.CustomAppDev.RecruitingManager.Web/App_Start/BundleConfig.cs
+++ Slalom.CustomAppDev.RecruitingManager.Web/App_Start/BundleConfig.cs
@@ -11,8 +11,11 @@ namespace Slalom.CustomAppDev.RecruitingManager.Web
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-                        "~/Scripts/jquery-ui-{version}.js"));
+                        "~/Scripts/jquery-ui.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include("~/Scripts/bootstrap.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.unobtrusive*",
@@ -24,6 +27,10 @@ namespace Slalom.CustomAppDev.RecruitingManager.Web
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include("~/Content/bootstrap.min.css"));
+            bundles.Add(new StyleBundle("~/Content/bootstrapResponsive").Include("~/Content/bootstrap-responsive.min.css"));                                
+          
+            
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
