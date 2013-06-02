783fe3874fa1fbc241537f6822f0a43c09949b4a
mhodgdon <mhodgdon@gmail.com>
2013-06-01 21:47:14 -0400

    Added tests for Candidate Controller

--- Slalom.CustomAppDev.RecruitingManager.UnitTests/API/CandidateControllerTests.cs
+++ Slalom.CustomAppDev.RecruitingManager.UnitTests/API/CandidateControllerTests.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Slalom.CustomAppDev.RecruitingManager.API.Controllers;
 using Slalom.CustomAppDev.RecruitingManager.DomainObjects.Candidate;
+using Moq;
+using Slalom.CustomAppDev.RecruitingManager.Commons.DataRepositories;
 
 namespace Slalom.CustomAppDev.RecruitingManager.UnitTests.API
 {
@@ -13,7 +15,12 @@ namespace Slalom.CustomAppDev.RecruitingManager.UnitTests.API
     {
         #region Fields
         
-        private CandidateController candidateController = null;
+        private CandidateController candidateController;
+        private Mock<ICandidateDataRepository> mockedCandidatedDataRepository;
+        private List<CandidateDomainObject> manyCandidates;
+        private List<CandidateDomainObject> nullCandidates;
+        private List<CandidateDomainObject> emptyCandidate;
+        private CandidateDomainObject candidateObject;
 
         #endregion
 
@@ -22,48 +29,125 @@ namespace Slalom.CustomAppDev.RecruitingManager.UnitTests.API
         [TestInitialize]
         public void Setup()
         {
-            candidateController = new CandidateController();
+            mockedCandidatedDataRepository = new Mock<ICandidateDataRepository>();
+            candidateController = new CandidateController(mockedCandidatedDataRepository.Object);
+
+            candidateObject = new CandidateDomainObject()
+            {
+                FirstName = "test",
+                LastName = "user",
+                Id = 1,
+                EmailAddress = "tuser@slalom.com",
+                HireCandidate = true,
+                InterviewComplete = true,
+                ResumeAddress = new Uri("http://resume.com/tuser")
+            };
+
+            manyCandidates = new List<CandidateDomainObject>()
+            {
+                new CandidateDomainObject() { Id = 99 },
+                new CandidateDomainObject() { Id = 101 }
+            };
+
+            emptyCandidate = new List<CandidateDomainObject>();
+
+            nullCandidates = null;
         }
 
         [TestCleanup]
         public void Teardown()
         {
             candidateController = null;
+            mockedCandidatedDataRepository = null;
         }
 
         #endregion
 
-        #region Test Methods
+        #region Get Test Methods
 
         [TestMethod]
         public void GetTestReturnsResults()
         {
+            mockedCandidatedDataRepository.Setup(mock => mock.RetrieveAllCandidates()).Returns(manyCandidates);
+
             IEnumerable<CandidateDomainObject> candidates = candidateController.Get();
 
-            Assert.IsNotNull(candidates);
-
-            Assert.IsTrue(candidates.Count() > 0);
+            mockedCandidatedDataRepository.Verify(mock => mock.RetrieveAllCandidates());
         }
-
 
         [TestMethod]
         public void GetTestReturnsNullResults()
         {
+            mockedCandidatedDataRepository.Setup(mock => mock.RetrieveAllCandidates()).Returns(nullCandidates);
+
             IEnumerable<CandidateDomainObject> candidates = candidateController.Get();
 
-            Assert.IsNull(candidates);
-
+            mockedCandidatedDataRepository.Verify(mock => mock.RetrieveAllCandidates());
         }
 
         [TestMethod]
         public void GetTestReturnsEmptyResults()
         {
+            mockedCandidatedDataRepository.Setup(mock => mock.RetrieveAllCandidates()).Returns(emptyCandidate);
+
             IEnumerable<CandidateDomainObject> candidates = candidateController.Get();
 
-            Assert.IsNotNull(candidates);
-
-            Assert.IsTrue(candidates.Count() == 0);
+            mockedCandidatedDataRepository.Verify(mock => mock.RetrieveAllCandidates());
         }
+
+        #endregion
+
+        #region Get(int) Test Methods
+
+        [TestMethod]
+        public void GetReturnsResponseFromGoodId()
+        {
+            mockedCandidatedDataRepository.Setup(mock => mock.RetrieveCandidate(candidateObject.Id)).Returns(candidateObject);
+
+            CandidateDomainObject candidate = candidateController.Get(candidateObject.Id);
+
+            mockedCandidatedDataRepository.Verify(mock => mock.RetrieveCandidate(candidateObject.Id));
+        }
+
+        [TestMethod]
+        public void GetReturnsEmptyObjectFromBadId()
+        {
+            candidateObject = new CandidateDomainObject() { FirstName = "UNDEFINED USER", LastName = "UNDEFINED USER", Id = 52 };
+
+            mockedCandidatedDataRepository.Setup(mock => mock.RetrieveCandidate(candidateObject.Id)).Returns(candidateObject);
+
+            CandidateDomainObject candidate = candidateController.Get(candidateObject.Id);
+
+            mockedCandidatedDataRepository.Verify(mock => mock.RetrieveCandidate(candidateObject.Id));
+        }
+
+        #endregion
+
+        #region Put Test Methods
+
+        [TestMethod]
+        public void PutExecutesWithValidCandidateObject()
+        {
+            mockedCandidatedDataRepository.Setup(mock => mock.CreateCandidate(candidateObject));
+
+            candidateController.Put(candidateObject);
+
+            mockedCandidatedDataRepository.Verify(mock => mock.CreateCandidate(candidateObject));
+       
+        }
+
+        [TestMethod]
+        public void PutExecutesWithInvalidCandidateObject()
+        {
+            candidateObject = null;
+
+            mockedCandidatedDataRepository.Setup(mock => mock.CreateCandidate(candidateObject));
+
+            candidateController.Put(candidateObject);
+
+            mockedCandidatedDataRepository.Verify(mock => mock.CreateCandidate(candidateObject));
+        }
+
 
         #endregion
     }
